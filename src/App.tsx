4a2c2e7556f5070ac90c00aa44e78fb951817342
atlas <a.sproson@outlook.com>
2024-01-26 14:56:07 +0000

    Initial grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
-
 function App() {
   return (
-    <div>
-      <p className=" text-blue-500">hello world</p>
+    <div className="grid min-h-screen place-items-center">
+      <ul className="flex gap-4 w-full max-w-6xl">
+        {[...Array(4).keys()].map((item) => (
+          <li className="bg-rose-300 h-[500px] w-full"></li>
+        ))}
+      </ul>
     </div>
   );
 }
